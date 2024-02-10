56c6bfc14e2b310a1fe6710af8a6e001a3d85ccd
Tanel Hiob <tanelhiob@outlook.com>
2024-02-10 19:08:33 +0200

    Add AI

--- BroomWorks.Rental.Web/Program.cs
+++ BroomWorks.Rental.Web/Program.cs
@@ -1,5 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+{
+    ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World, CI/CD is a GO <3 !!");
